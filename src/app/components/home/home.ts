42152ea04af49156548fc35e46604dc038ad4dca
Gabriel Garcia Vazquez <gabriel.garciavazquez7@gmail.com>
2026-09-09 16:25:31 -0300

    Modificacion Home

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,4 +1,5 @@
-import { Component, inject } from '@angular/core';
+import { Component, DestroyRef, inject, PLATFORM_ID, signal } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { Auth } from '../../services/auth';
 
@@ -12,4 +13,17 @@ export class Home {
   private readonly auth = inject(Auth);
 
   readonly usuario = this.auth.usuario;
+
+  /** Imágenes de fondo del home (en public/img/) */
+  readonly imagenes = ['/img/home-1.jpg', '/img/home-2.jpg', '/img/home-3.jpg'];
+  readonly slide = signal(0);
+
+  constructor() {
+    if (isPlatformBrowser(inject(PLATFORM_ID))) {
+      const id = setInterval(() => {
+        this.slide.update((i) => (i + 1) % this.imagenes.length);
+      }, 2000);
+      inject(DestroyRef).onDestroy(() => clearInterval(id));
+    }
+  }
 }
